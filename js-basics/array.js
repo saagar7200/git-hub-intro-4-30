f14fdfd731d2d42848c495c61716aef384968f1b
Sagar Bhandari <rsaagar7200@gmail.com>
2025-06-11 17:29:13 +0545

    array

---
+++ js-basics/array.js
@@ -0,0 +1,120 @@
+
+// // let numbers = [1,2,3,4]
+// // // {}
+// // // let arr = new Array()
+
+// // // read 
+// // console.log(numbers[0])
+// // // console.log(numbers[numbers.length-1])
+
+// // // update
+// // numbers[0] = 10
+
+// // // add 
+// // // end
+// // numbers.push(5)
+// // //start
+// // numbers.unshift(60)
+
+// // // remove
+// // // end
+// //  const lastEl = numbers.pop()
+// // // start
+// // const firstEl = numbers.shift()
+// // console.log(lastEl,firstEl)
+
+// // console.log(numbers.length)
+
+// // // at
+
+// // // console.log(numbers.at(-1))
+
+
+// // 
+// const numbers = [45,23,4,5,6,7,4]
+
+// // user.role = 'user'
+// // allowedRoles = []
+
+// // array methods
+
+// // query 
+// // includes indexOf
+// // console.log(numbers.includes(450))
+// // console.log(numbers.lastIndexOf(4))
+
+
+// // call back
+
+// // find findIndex
+
+// const result = numbers.find((value) => value > 4)
+
+// const index = numbers.findIndex((value,)=> value > 4)
+
+// // console.log(result,index)
+
+
+// // 
+// // loop 
+// // const value =  numbers.forEach((value,index,arr ) =>{
+// //      console.log(value)
+// //     //  arr[index] = ''
+// //      return value
+// // } )
+
+// // console.log(value)
+
+// // map
+// // returns an array
+// // [A] => [B]
+
+// // [45,23,4,5,6,7,4] => [90,46]
+// // const doubled = numbers.map((value,index,arr ) =>{
+// //     return 'x'
+// // })
+
+// // filter 
+// // const even = numbers.filter(value => value % 2 !== 0)
+
+
+// // reduce
+
+// // const sum = numbers.reduce((acc,value,index,arr) =>{
+// //     return acc + value
+// // },0)
+
+
+// // slice
+// const newArr = numbers.slice(1,3)
+
+// // delete numbers[3]
+
+// // splice
+
+// // numbers.splice(start,deleteCount,item1,item2...)
+// console.log(numbers)
+
+// // numbers.splice(2,0,67,45)
+
+
+// // numbers.reverse()
+
+// // sort
+
+// const numbers = [45,23,5,4,6,7,4,101]
+// const fruits = ['grapes','apples','kiwi']
+
+// numbers.sort((a,b)=>{
+//   return b - a
+// })
+// fruits.sort()
+
+// console.log(numbers,fruits)
+// console.log(newArr)
+
+// const arr = [[1,2],[3,4]]
+// const flat = arr.flat()
+// console.log(flat)
+
+
