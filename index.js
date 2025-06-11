0955eccc0957b69530de676e71dd6ef909bb6be6
Sagar Bhandari <rsaagar7200@gmail.com>
2025-06-11 17:18:49 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+// let var const
+// int float char
+
+// int a = 10
+
+let x = 10
+//  x ='hg'
+
+console.log(x)
+
+// const 
+const y =20
+var z =20
+// y=10
+// let
+x =20
+var z = 30
+
+console.log(x,z)
+
+
