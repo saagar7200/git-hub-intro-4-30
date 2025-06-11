ec01310f5d4a274364a1089925eddc914fce6498
Sagar Bhandari <rsaagar7200@gmail.com>
2025-06-11 17:40:46 +0545

    git commands

--- index.js
+++ index.js
@@ -30,3 +30,14 @@ console.log(x,z)
 // git checkout -b <branch_name> // to create new branch and checkout
 // git checkout <branch_name> 
 
+
+// remote
+// git remote add origin <git_hub_repo_link>
+// git remote -v 
+// git remote remove origin
+
+// changes   git add         commit
+// wd -> staging_area -> local repo.
+
+
+// git push 
